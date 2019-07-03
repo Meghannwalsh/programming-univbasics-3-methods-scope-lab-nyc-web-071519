f924b754d01eb04b031f10511cee711786d9843c
Meghann McCool Walsh <meghann.mccool.walsh@gmail.com>
2019-07-03 14:49:14 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,5 @@
 def mario
-  
+
   phrase = "It's-a me, Mario!"
   puts phrase
 end
@@ -8,3 +8,7 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  puts "It's Dangerous To Go Alone! Take This."
+end 
